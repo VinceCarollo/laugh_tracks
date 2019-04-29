c4f8865bffcface2e191605dc42375b70c48118b
Vince Carollo <vincecarollo@gmail.com>
2019-04-29 17:23:43 -0600

    Set up controller and view page for showing individual comedians

--- app/controllers/comedians_controller.rb
+++ app/controllers/comedians_controller.rb
@@ -2,4 +2,8 @@ class ComediansController < ApplicationController
     def index 
         @comedians = Comedian.all
     end
+
+    def show 
+        @comedian = Comedian.find(params[:id])
+    end
 end
