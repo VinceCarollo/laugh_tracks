8bee4c6030f94a5684b6596a076a69c7d360b9fd
Vince Carollo <vincecarollo@gmail.com>
2019-04-30 12:43:01 -0600

    Add line to destroy all specials in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 
 Comedian.destroy_all
+Special.destroy_all
 
 Comedian.create(name: "Bill Burr", age: 50, birthplace: "Canton, MA", image_url: "https://image.masslive.com/home/mass-media/width960/img/entertainment/photo/nashville-comedy-festival-21d347107e975672.jpg")
 Comedian.create(name: "Joe Rogan", age: 51, birthplace: "Newark, NJ", image_url: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwil2d6_-YvhAhUR0IMKHSTACf4QjRx6BAgBEAU&url=https%3A%2F%2Fwaaf.radio.com%2Fblogs%2Fanthony-capobianco%2F13-rockers-have-appeared-joe-rogan-experience&psig=AOvVaw1DfyJPblSkSVLayC7G5let&ust=1553007377857892")
