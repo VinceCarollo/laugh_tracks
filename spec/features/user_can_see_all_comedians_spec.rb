2ff281b25d5b24737fa17b98d4e08f79cf5aecaa
Vince Carollo <vincecarollo@gmail.com>
2019-04-30 18:32:17 -0600

    Add specials to comedians index view

--- spec/features/user_can_see_all_comedians_spec.rb
+++ spec/features/user_can_see_all_comedians_spec.rb
@@ -4,10 +4,10 @@ describe 'when user goes to comedian page' do
     it 'shows all comedian attributes' do
         ray = Comedian.create!(name: 'Ray', age: 22, birthplace: 'New York')
         jon = Comedian.create!(name: 'Jon', age: 32, birthplace: 'Boston')
-        special_1 = Special.create!(name: 'Chew on This', runtime_mins: 22, commedian_id: ray.id)
-        special_2 = Special.create!(name: 'Forever Not Funny', runtime_mins: 41, commedian_id: ray.id)
-        special_3 = Special.create!(name: 'All for the Money', runtime_mins: 42, commedian_id: jon.id)
-        special_4 = Special.create!(name: 'Live and Livin', runtime_mins: 64, commedian_id: jon.id)
+        special_1 = Special.create!(name: 'Chew on This', runtime_mins: 22, comedian_id: ray.id)
+        special_2 = Special.create!(name: 'Forever Not Funny', runtime_mins: 41, comedian_id: ray.id)
+        special_3 = Special.create!(name: 'All for the Money', runtime_mins: 42, comedian_id: jon.id)
+        special_4 = Special.create!(name: 'Live and Livin', runtime_mins: 64, comedian_id: jon.id)
 
         visit comedians_path
 
