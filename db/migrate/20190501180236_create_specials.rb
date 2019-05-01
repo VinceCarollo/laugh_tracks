639e68e2012708cf1abb7dfb0a484fe40c84aa59
Vince Carollo <vincecarollo@gmail.com>
2019-05-01 14:11:27 -0600

    Delete tables in database and remigrate them for a better relationship between them

--- db/migrate/20190501180236_create_specials.rb
+++ db/migrate/20190501180236_create_specials.rb
@@ -3,7 +3,6 @@ class CreateSpecials < ActiveRecord::Migration[5.1]
     create_table :specials do |t|
       t.string :name
       t.integer :runtime_mins
-      t.integer :comedian_id
       t.string :image_url
     end
   end
