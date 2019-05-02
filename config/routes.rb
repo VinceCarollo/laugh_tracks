2f947f6c1bc7da9560ca545265effebc719cd5bd
Vince Carollo <vincecarollo@gmail.com>
2019-05-02 12:43:20 -0600

    Hand Roll routes needed and assigned path comedian to show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :comedians
+  # resources :comedians
+  get '/comedians', to: 'comedians#index'
+  get '/comedians/:id', to: 'comedians#show', as: 'comedian'
 end
