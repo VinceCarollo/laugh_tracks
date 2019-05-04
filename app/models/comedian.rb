3160109b3bcf7fe04143a4d566b2c95139232f0e
Vince Carollo <vincecarollo@gmail.com>
2019-05-04 16:51:08 -0600

    Refactor comedian model, views, and controller to follow mvc conventions

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,4 +1,20 @@
 class Comedian < ApplicationRecord
   has_many :specials, dependent: :destroy
   validates_presence_of :name, :age, :birthplace
+
+  def special_count
+    self.specials.count
+  end
+
+  def self.get_comedians_by_age(age)
+    self.where("age = #{age}")
+  end
+
+  def self.get_all_comedians
+    self.all
+  end
+
+  def self.get_comedian_by_id(id)
+    self.find(id)
+  end
 end
