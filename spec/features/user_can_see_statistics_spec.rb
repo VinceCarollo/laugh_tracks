7ac818e2b92960978b3711424cd02329bf6b3ecd
Vince Carollo <vincecarollo@gmail.com>
2019-05-05 14:03:54 -0600

    Add feature tests for statistics - all specials count and average runtime length

--- spec/features/user_can_see_statistics_spec.rb
+++ spec/features/user_can_see_statistics_spec.rb
@@ -13,6 +13,9 @@ RSpec.describe 'As a visitor' do
         jon = Comedian.create!(name: 'Jon', age: 32, birthplace: 'Boston')
         jason = Comedian.create!(name: 'Jason', age: 27, birthplace: 'Kansas City')
 
+        special_1 = Special.create!(name: 'Chew on This', runtime_mins: 22, comedian: ray)
+        special_3 = Special.create!(name: 'All for the Money', runtime_mins: 42, comedian: jon)
+
         visit comedians_path
 
         within '.statistics' do
@@ -31,6 +34,9 @@ RSpec.describe 'As a visitor' do
         jon = Comedian.create!(name: 'Jon', age: 32, birthplace: 'Boston')
         jason = Comedian.create!(name: 'Jason', age: 22, birthplace: 'Kansas City')
 
+        special_1 = Special.create!(name: 'Chew on This', runtime_mins: 22, comedian: ray)
+        special_3 = Special.create!(name: 'All for the Money', runtime_mins: 42, comedian: jon)
+
         visit '/comedians?age=22'
 
         within '.statistics' do
@@ -38,6 +44,26 @@ RSpec.describe 'As a visitor' do
           expect(page).to have_content("New York")
           expect(page).to_not have_content("Boston")
           expect(page).to have_content("Kansas City")
+        end
+
+      end
+
+      it 'contains additional statistics - total special count and avg run length' do
+        ray = Comedian.create!(name: 'Ray', age: 22, birthplace: 'New York')
+        jon = Comedian.create!(name: 'Jon', age: 32, birthplace: 'Boston')
+
+        special_1 = Special.create!(name: 'Chew on This', runtime_mins: 22, comedian: ray)
+        special_2 = Special.create!(name: 'Forever Not Funny', runtime_mins: 41, comedian: ray)
+        special_3 = Special.create!(name: 'All for the Money', runtime_mins: 42, comedian: jon)
+        special_4 = Special.create!(name: 'Live and Livin', runtime_mins: 64, comedian: jon)
+
+        visit comedians_path
+
+        within '.statistics' do
+          expect(page).to have_content('All Specials Count:')
+          expect(page).to have_content(4)
+          expect(page).to have_content('Average Special Run Length:')
+          expect(page).to have_content(42.3)
         end
 
       end
