8fdf19750f973d5314fdccb00c71ae7e3271bc46
Vince Carollo <vincecarollo@gmail.com>
2019-05-05 11:15:06 -0600

    Change root route to comedians page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # resources :comedians
+  root to: 'comedians#index'
+
   get '/comedians', to: 'comedians#index'
   get '/comedians/new', to: 'comedians#new'
   post '/comedians', to: 'comedians#create'
